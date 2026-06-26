d996e03b4d27c0d0c67a94adc944ee3c02d3dcd3
txx <peri_111@qq.com>
2026-06-26 14:19:07 +0800

    3

--- ui_tests/run.py
+++ ui_tests/run.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
 
     # ✅ 关键修改：output 直接指定完整文件路径，这样文件名就是 result.xml
     runner = xmlrunner.XMLTestRunner(
-        output='reports/result.xml'
+        output='E:/rjcs/automatedtest/ui_tests/reports/result.xml'
     )
     result = runner.run(suite)
 
